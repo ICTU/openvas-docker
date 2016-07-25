5594bd023ac0d6466d13f153be32ea388f80192d
Mr.Breaker <rory.breuk@ictu.nl>
2016-07-25 10:53:38 +0200

    Committed changes to original scripts

--- bin/start.sh
+++ bin/start.sh
@@ -29,12 +29,3 @@ echo "Checking setup"
 
 /openvas/openvas-check-setup --v8 --server;
 echo "Done."
-
-echo "Starting infinite loop..."
-
-echo "Press [CTRL+C] to stop.."
-
-while true
-do
-	sleep 1
-done
