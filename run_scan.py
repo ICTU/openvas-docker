a3465d395e4c0a7de2dfd4283d7513cde46f2af8
Frank Niessink <frank@niessink.com>
2019-04-03 13:23:43 +0200

    Write both XML and HTML report.

--- run_scan.py
+++ run_scan.py
@@ -5,14 +5,15 @@ import os, sys, time
 from lxml import etree
 import subprocess
 
-configs = [ "8715c877-47a0-438d-98a3-27c7a6ab2196", #  Discovery
-		"085569ce-73ed-11df-83c3-002264764cea", #  empty
-		"daba56c8-73ec-11df-a475-002264764cea", #  Full and fast
-		"698f691e-7489-11df-9d8c-002264764cea", #  Full and fast ultimate
-		"708f25c4-7489-11df-8094-002264764cea", #  Full and very deep
-		"74db13d6-7489-11df-91b9-002264764cea", #  Full and very deep ultimate
-		"2d3f051c-55ba-11e3-bf43-406186ea4fc5", #  Host Discovery
-		"bbca7412-a950-11e3-9109-406186ea4fc5" ] #  System Discovery
+configs = [
+    "8715c877-47a0-438d-98a3-27c7a6ab2196", #  Discovery
+    "085569ce-73ed-11df-83c3-002264764cea", #  empty
+    "daba56c8-73ec-11df-a475-002264764cea", #  Full and fast
+    "698f691e-7489-11df-9d8c-002264764cea", #  Full and fast ultimate
+    "708f25c4-7489-11df-8094-002264764cea", #  Full and very deep
+    "74db13d6-7489-11df-91b9-002264764cea", #  Full and very deep ultimate
+    "2d3f051c-55ba-11e3-bf43-406186ea4fc5", #  Host Discovery
+    "bbca7412-a950-11e3-9109-406186ea4fc5" ] #  System Discovery
 
 if len(sys.argv) < 3:
     sys.exit('Usage: %s <scan target> <output file>' % sys.argv[0])
@@ -58,17 +59,22 @@ openvaslog = open("/var/log/openvas/openvassd.messages", "r").read()
 print("openvassd.messages: {}".format(openvaslog))
 report_id = etree.XML(get_status_response).xpath("//report")[0].get("id")
 print("report_id: {}".format(report_id))
-get_report = "omp {} -R {} -f 6c248850-1f62-11e1-b082-406186ea4fc5 --details".format(omp_logon, report_id)
-report_response = subprocess.check_output(get_report, stderr=subprocess.STDOUT, shell=True)
-print("report: {}".format(report_response[:30]))
 
-report_filename = os.path.split(sys.argv[2])[1]
-export_path = "/openvas/results/" + report_filename 
-print('Writing HTML report to ' + export_path)
+report_formats = [("html", "6c248850-1f62-11e1-b082-406186ea4fc5"), ("xml", "a994b278-1f62-11e1-96ac-406186ea4fc5")]
+for report_format, report_format_id in report_formats:
+    get_report = "omp {} -R {} -f {}".format(omp_logon, report_id, report_format_id)
+    report_response = subprocess.check_output(get_report, stderr=subprocess.STDOUT, shell=True)
+    print("{}-report: {}...".format(report_format.upper(), report_response[:30]))
 
-f = open(export_path, 'w')
-f.write(report_response)
-f.close()
+    report_filename = os.path.split(sys.argv[2])[1]
+    if report_filename.ensdwith(".html"):
+        report_filename = report_filename[:-len(".html")]
+    export_path = "/openvas/results/{}.{}".format(report_filename, report_format)
+    print('Writing {}-report to {}'.format(report_format.upper(), export_path))
+
+    f = open(export_path, 'w')
+    f.write(report_response)
+    f.close()
 
 print('Done!')
 
